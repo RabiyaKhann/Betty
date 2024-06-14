1e12c5195f96be9a618cefb569fa44a3020af2c8
RabiyaKhann <90935572+RabiyaKhann@users.noreply.github.com>
2024-06-15 03:34:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
@@ -10,6 +11,7 @@ model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
 
 
 app = Flask(__name__)
+CORS(app) 
 
 @app.route("/")
 def index():
