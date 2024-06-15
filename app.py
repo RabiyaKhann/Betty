7fd8acfd3baa42cda7ca55a3f9767c837aed5707
RabiyaKhann <90935572+RabiyaKhann@users.noreply.github.com>
2024-06-15 05:49:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,42 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
-
-
-from transformers import AutoModelForCausalLM, AutoTokenizer
+import os
 import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
+app = Flask(__name__)
+CORS(app)
 
+# Initialize tokenizer and model
 tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
 model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
 
-
-app = Flask(__name__)
-CORS(app) 
+# Define a variable to store chat history
+chat_history_ids = None
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-
 @app.route("/get", methods=["GET", "POST"])
 def chat():
-    msg = request.form["msg"]
-    input = msg
-    return get_Chat_response(input)
-
+    if request.method == "POST":
+        msg = request.form["msg"]
+        response = get_Chat_response(msg)
+        return jsonify({"response": response})
+    else:
+        return "Method not allowed", 405
 
 def get_Chat_response(text):
+    global chat_history_ids
 
-    # Let's chat for 5 lines
-    for step in range(5):
-        # encode the new user input, add the eos_token and return a tensor in Pytorch
-        new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
+    # Encode user input and generate response
+    new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
+    bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if chat_history_ids is not None else new_user_input_ids
+    chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+    response = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
 
-        # append the new user input tokens to the chat history
-        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
-
-        # generated a response while limiting the total chat history to 1000 tokens, 
-        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
-
-        # pretty print last ouput tokens from bot
-        return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
-
+    return response
 
 if __name__ == '__main__':
     app.run(port=int(os.environ.get('PORT', 5000)))
